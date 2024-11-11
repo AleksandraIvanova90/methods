825e08eceda452064273e45cb5973ccc968f0d43
Aleksandra Ivanova <sashaivanova90@mail.ru>
2024-11-11 14:20:29 +0300

    change classes

--- src/js/class/Character.js
+++ src/js/class/Character.js
@@ -1,7 +1,7 @@
 
 
 export default class Character {
-    /*eslint no-unused-vars: ["error", { "args": "none" }]*/
+   
     constructor(name, type, health, level, attack, defence) {
         if (typeof name == 'number' || name.length < 2 || name.length > 4) {
             throw new Error("Кол-во символов в name от 2 до 4!")
@@ -11,36 +11,12 @@ export default class Character {
         }
         this.name = name;
         this.type = type;
-        this.health = 100;
-        this.level = 1;
-
-        switch (type) {
-            case 'Bowman':
-                this.attack = 25;
-                this.defence = 25;
-                break
-            case 'Swordsman':
-                this.attack = 40;
-                this.defence = 10;
-                break
-            case 'Magician':
-                this.attack = 10;
-                this.defence = 40;
-                break
-            case 'Daemon':
-                this.attack = 10;
-                this.defence = 40;
-                break
-            case 'Undead':
-                this.attack = 25;
-                this.defence = 25;
-                break
-            case 'Zombie':
-                this.attack = 40;
-                this.defence = 10;
-                break
-        }
+        this.health = health;
+        this.level = level;
+        this.attack = attack;
+        this.defence = defence;
     }
+
 
     levelUp() {
         if (this.health === 0) {
--- src/js/class/Daemon.js
+++ src/js/class/Daemon.js
@@ -3,5 +3,9 @@ import Character from '../class/Character'
 export default class Daemon extends Character {
     constructor (name) {
         super(name,'Daemon');
+        this.level = 1;
+        this.health = 100;
+        this.attack = 10;
+        this.defence = 40;
     }
 }
